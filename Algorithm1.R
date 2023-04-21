0a8ebc9e111b3d9c8d7b7db4a83810db514cd9c1
matt4ta <57694978+matt4ta@users.noreply.github.com>
2023-04-20 23:09:59 -0400

    Update Algorithm1.R
    Changed "=" to "<-". Not sure if that actually matters though

--- Algorithm1.R
+++ Algorithm1.R
@@ -8,28 +8,28 @@ library(parallelMap)
 parallelStartMulticore(4)
 
 # Load training data
-my_data = read.csv("train.csv")
+my_data <- read.csv("train.csv")
 
 # Define task
-task = makeClassifTask(data = my_data, target = "label")
+task <- makeClassifTask(data = my_data, target = "label")
 
 # Define learner
-learner = makeLearner("classif.randomForest")
+learner <- makeLearner("classif.randomForest")
 
 # Train model
-model = train(learner, task)
+model <- train(learner, task)
 
 # Load image data to classify
-image = readJPEG("image.jpg")
+image <- readJPEG("image.jpg")
 
 # Convert image to data frame
-image_df = data.frame(t(matrix(as.vector(image), ncol = 3)))
+image_df <- data.frame(t(matrix(as.vector(image), ncol = 3)))
 
 # Predict class of image
-prediction = predict(model, newdata = image_df)
+prediction <- predict(model, newdata = image_df)
 
 # Convert prediction to character
-prediction = as.character(prediction)
+prediction <- as.character(prediction)
 
 # Save predicted class to file
 writeLines(prediction, "predicted_class.txt")
