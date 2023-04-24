ae2ad4715d7b786ef5c098f06ed20dbe144ed8e1
matt4ta <57694978+matt4ta@users.noreply.github.com>
2023-04-24 10:45:55 -0400

    model work now
    work

--- orig_model.R
+++ orig_model.R
@@ -2,6 +2,9 @@ library(tidyverse)
 library(keras)
 library(tensorflow)
 library(reticulate)
+library(here)
+
+setwd(here())
 
 ## RUN SEPARATELY ##
 install_tensorflow(extra_packages="pillow")
@@ -9,8 +12,26 @@ install_tensorflow(extra_packages="pillow")
 ## RUN SEPARATELY ##
 install_keras()
 
+model <- load_model_tf("model/dandelion_model")
+
 res=c("","")
-f=list.files("./grass")
+width <- 224
+height<- 224
+target_size <- c(width, height)
+rgb <- 3 #color channels
+
+
+
+f_grass <- list.files("data-for-332/grass")
+
+test_image <- image_load(paste("data-for-332/grass/",f_grass[1],sep=""),
+                         target_size = target_size)
+x <- image_to_array(test_image)
+x <- array_reshape(x, c(1, dim(x)))
+x <- x/255
+pred <- model %>% predict(x)
+print(pred)
+
 for (i in f){
   test_image <- image_load(paste("./grass/",i,sep=""),
                            target_size = target_size)
@@ -24,7 +45,7 @@ for (i in f){
 }
 
 res=c("","")
-f=list.files("./dandelions")
+f_dande <- list.files(here("data-for-332/dandelions"))
 for (i in f){
   test_image <- image_load(paste("./dandelions/",i,sep=""),
                            target_size = target_size)
