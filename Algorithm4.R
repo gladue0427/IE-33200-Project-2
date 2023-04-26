84e2e41f10d53cf9576f25c6eaf684149f8e7771
matt4ta <57694978+matt4ta@users.noreply.github.com>
2023-04-26 16:34:34 -0400

    made more usable

---
+++ Algorithm4.R
@@ -0,0 +1,25 @@
+change_most_average <- function(y, pixel_budget=0.01) {
+  
+  # averages for each color value in image
+  mean_R <- mean(y[,,1])
+  mean_G <- mean(y[,,2])
+  mean_B <- mean(y[,,3])
+  
+  # calculates how far away each pixel's color is from the average image color
+  dist_from_avg <- abs(y[,,1] - mean_R) + abs(y[,,2] - mean_G) + abs(y[,,3] - mean_B)
+  
+  # finds indices of the most average pixels, limited to the pixel budget
+  cutoff <- quantile(dist_from_avg, pixel_budget, type=1)
+  most_avg <- which(dist_from_avg < cutoff, arr.ind = TRUE)
+  
+  # changes colors of the most average pixels
+  for (i in 1:dim(most_avg)[1]) {
+    pixel <- most_avg[i,]
+    
+    ############### Make pixel a random color #########
+    y[pixel[1], pixel[2], 1] <- runif(1)
+    y[pixel[1], pixel[2], 2] <- runif(1)
+    y[pixel[1], pixel[2], 3] <- runif(1)
+  }
+  return(y)
+}
--- orig_model.R
+++ orig_model.R
@@ -2,9 +2,7 @@ library(tidyverse)
 library(keras)
 library(tensorflow)
 library(reticulate)
-library(here)
-
-setwd("~/GitHub/IE-33200-Project-2")
+#library(here)
 
 ## RUN SEPARATELY ##
 install_tensorflow(extra_packages="pillow")
@@ -12,16 +10,16 @@ install_tensorflow(extra_packages="pillow")
 ## RUN SEPARATELY ##
 install_keras()
 
+############ RUN ALL THESE LINES BEFORE USING MODEL ####################
+setwd("~/GitHub/IE-33200-Project-2")
+source("Main_Algorithm.R")
+source("Algorithm1.R")
+source("Algorithm2.R")
+source("Algorithm3.R")
+source("Algorithm4.R")
+source("Algorithm5.R")
 
-
-
-# this whole file is pretty messy I'll clean it up tomorrow
-
-
-
-
-
-
+# run separately - may take a long time
 model <- load_model_tf("dandelion_model")
 
 res=c("","")
@@ -33,39 +31,70 @@ rgb <- 3 #color channels
 f_grass <- list.files("grass")
 f_dande <- list.files("dandelions")
 
-############################## TEST CODE ############################
-test_image <- image_load("grass",
-                         target_size = target_size)
 
-# test_image <- image_load("dandelions",
-#                          target_size = target_size)
+############################## SINGLE GRASS IMAGE ############################
 
-#y <- image_load(paste("grass/",f_grass[1],sep=""), target_size = target_size)
-y <- image_load(paste("dandelions/",f_dande[1],sep=""),
-                target_size = target_size)
-y <- image_to_array(y)
-y <- y/255
+# upload original image - change image index to use new picture
+grass_image_index <- 1
+orig_grass_name <-paste("grass/",f_grass[grass_image_index],sep="")
+orig_grass <- image_load(orig_grass_name, target_size = target_size)
+orig_grass <- image_to_array(orig_grass)
+orig_grass <- orig_grass/255
 
-source("algos.R")
-############### Find "most green" pixels in image ####################
-pixel_budget <- 0.01
+# plot original image
+plot(1:dim(orig_grass)[1], 1:dim(orig_grass)[2], main=paste("Grass Image Index: ", grass_image_index,"\nOriginal"),type = "n", xlab = "", ylab = "", axes = FALSE)
+rasterImage(as.raster(orig_grass), 1, 1, dim(orig_grass)[1], dim(orig_grass)[2])
 
-y_inv <- mod_image(y, 0.1)
-# y_inv <- change_least_average(y)
-plot(1:dim(y)[1], 1:dim(y)[2], type = "n", xlab = "", ylab = "", axes = FALSE)
-rasterImage(as.raster(y_inv), 1, 1, dim(y)[1], dim(y)[2])
-y_test <- array_reshape(y_inv, c(1, dim(y_inv)))
-model %>% predict(y_test)
+# modify image 
+pixel_budget <- 0.05 #percentage of pixels to change
+mod_grass <- mod_image(orig_grass, pixel_budget=pixel_budget)
 
-#############################################################################
+# plot modified image
+plot(1:dim(mod_grass)[1], 1:dim(mod_grass)[2], main=paste("Image Index: ", grass_image_index, "\nModified with Pixel Budget = ", pixel_budget),type = "n", xlab = "", ylab = "", axes = FALSE)
+rasterImage(as.raster(mod_grass), 1, 1, dim(mod_grass)[1], dim(mod_grass)[2])
 
+# make prediction using model
+mod_grass_pred <- array_reshape(mod_grass, c(1, dim(mod_grass)))
+prediction <- model %>% predict(mod_grass_pred)
+print(prediction)
+
+############################## SINGLE DANDELION IMAGE ############################
+
+# upload original image - change image index to use new picture
+dan_image_index <- 1
+orig_dan_name <-paste("dandelions/",f_dande[dan_image_index],sep="")
+orig_dan <- image_load(orig_dan_name, target_size = target_size)
+orig_dan <- image_to_array(orig_dan)
+orig_dan <- orig_dan/255
+
+# plot original image
+plot(1:dim(orig_dan)[1], 1:dim(orig_dan)[2], main=paste("Dandelion Image Index: ", dan_image_index,"\nOriginal Image"),type = "n", xlab = "", ylab = "", axes = FALSE)
+rasterImage(as.raster(orig_dan), 1, 1, dim(orig_dan)[1], dim(orig_dan)[2])
+
+# modify image
+pixel_budget <- 0.05
+mod_dan <- mod_image(orig_dan, pixel_budget=pixel_budget)
+
+# plot modified image
+plot(1:dim(mod_dan)[1], 1:dim(mod_dan)[2], main=paste("Dandelion Image Index: ", dan_image_index,"\nModified with Pixel Budget = ", pixel_budget),type = "n", xlab = "", ylab = "", axes = FALSE)
+rasterImage(as.raster(mod_dan), 1, 1, dim(mod_dan)[1], dim(mod_dan)[2])
+
+# make prediction using model
+mod_dan_pred <- array_reshape(mod_dan, c(1, dim(mod_dan)))
+prediction <- model %>% predict(mod_dan_pred)
+print(prediction)
+
+####################### FINDING NUMBER OF IMAGES FOOLED ###############################
+
+# grass images
 num_fooled <- 0
+pixel_budget <- 0.05
 for (i in f_grass){
   test_image <- image_load(paste("grass/",i,sep=""),
                            target_size = target_size)
   x <- image_to_array(test_image)
   x <- x/255
-  x <- mod_image(x, 0.05)
+  x <- mod_image(x, pixel_budget = pixel_budget)
   x <- array_reshape(x, c(1, dim(x)))
   pred <- model %>% predict(x)
   if(pred[1,2]<0.50){
@@ -75,15 +104,18 @@ for (i in f_grass){
 }
 print(num_fooled)
 
+
+# dandelion images
 res=c("","")
-num_fooled = 0
+num_fooled <- 0
+pixel_budget <- 0.05
 for (i in f_dande){
   test_image <- image_load(paste("dandelions/",i,sep=""),
                            target_size = target_size)
   x <- image_to_array(test_image)
   x <- x/255
   #x <- convert_mostGreen(x)
-  x <- mod_image(x, pixel_budget = 0.05)
+  x <- mod_image(x, pixel_budget = pixel_budget)
   x_test <- array_reshape(x, c(1, dim(x)))
   pred <- model %>% predict(x_test)
   print(pred)
@@ -91,7 +123,7 @@ for (i in f_dande){
     # print(i)
     # plot(1:224, 1:224, type = "n")
     # rasterImage(as.raster(x), 1, 1, 224, 224)
-    num_fooled = num_fooled + 1
+    num_fooled <- num_fooled + 1
   }
 }
 print(num_fooled)
