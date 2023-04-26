8596ca0411d3dc750b92e3e8e6024605187996a4
matt4ta <57694978+matt4ta@users.noreply.github.com>
2023-04-26 00:38:54 -0400

    added README, commented code

--- algos.R
+++ algos.R
@@ -1,21 +1,19 @@
-reduceUpperExtremities <- function(x, pixel_budget) {
-  cutoff <- quantile(x, 1-pixel_budget, type=1)
-  print(cutoff)
-  ext_removed <- apply(x, 1, function(r){r[r > cutoff] <- 0})
-  return(ext_removed)
-}
-
+# converts most green pixels to a random color
 convert_mostGreen <- function(y, pixel_budget=0.01) {
   # calculate the "greenness" of the image
   greenNess <- y[,,2] - y[,,1] - y[,,3]
   
-  
+  # finds indices of the most green pixels, limited to pixel budget
   cutoff <- quantile(greenNess, 1 - pixel_budget, type=1)
   most_green <- which(greenNess > cutoff, arr.ind=TRUE)
   
   #return(most_green)
+  
+  # changes most green pixels to a random color
   for (i in 1:dim(most_green)[1]) {
     pixel <- most_green[i,]
+    
+    # make pixel a random color
     y[pixel[1], pixel[2], 1] <- runif(1)
     y[pixel[1], pixel[2], 2] <- runif(1)
     y[pixel[1], pixel[2], 3] <- runif(1)
@@ -24,6 +22,7 @@ convert_mostGreen <- function(y, pixel_budget=0.01) {
   return(y)
 }
 
+# converts most yellow pixels to the average color of the image
 convert_mostYellow <- function(y, pixel_budget=0.01) {
   mean_R <- mean(y[,,1])
   mean_G <- mean(y[,,2])
@@ -46,35 +45,28 @@ convert_mostYellow <- function(y, pixel_budget=0.01) {
     # y[pixel[1], pixel[2], 1] <- runif(1, 0, 0.1)
     # y[pixel[1], pixel[2], 2] <- runif(1, 0.3, 0.8)
     
-    ############### Make pixel a random color #########
-    # y[pixel[1], pixel[2], 1] <- runif(1)
-    # y[pixel[1], pixel[2], 2] <- runif(1)
-    # y[pixel[1], pixel[2], 3] <- runif(1)
-    
     
     ########Make Pixel Average Color of Image #######
     y[pixel[1], pixel[2], 1] <- mean_R
     y[pixel[1], pixel[2], 2] <- mean_G
     y[pixel[1], pixel[2], 3] <- mean_B
-    
-    ################ Make Pixels Yellow ###############
-    # y[pixel[1], pixel[2], 1:2] <- runif(1, 0.78, 1)
-    # 
-    # y[pixel[1], pixel[2], 3] <- 0
   }
   return(y)
 }
 
-
+# changes random pixels in image
 randomPixels <- function(x, pixel_budget = 0.01) {
-  mean_R <- mean(x[,,1])
-  mean_G <- mean(x[,,2])
-  mean_B <- mean(x[,,3])
   
+  # initialize matrix where the indices of the pixels to be changed will be stored
+  # # of rows = number of pixels to be changed
+  # first element of row is the row index of the pixel to be changed
+  # second element is the column index
   numRows <- dim(x)[1]
   numCols <- dim(x)[2]
   change_pixels <- matrix(0, nrow=numRows * numCols * pixel_budget, ncol = 2)
-  # change_pixels <- apply(change_pixels, 1, function(r){r[1] <- runif(1, min=1, max=numRows); r[2] <- runif(1, min=1, max=numCols)})
+  
+  # fills matrix with random values corresponding to 
+  # the number of rows or columns as appropriate
   change_pixels[,1] <- round(runif(dim(change_pixels)[1], min=1, max=numRows), 0)
   change_pixels[,2] <- round(runif(dim(change_pixels)[1], min=1, max=numCols), 0)
   
@@ -82,95 +74,75 @@ randomPixels <- function(x, pixel_budget = 0.01) {
   for (i in 1:dim(change_pixels)[1]) {
     pixel <- change_pixels[i,]
     
-    ################ Make Pixel Green-ish #############
-    # y[pixel[1], pixel[2], 1] <- runif(1, 0, 0.1)
-    # y[pixel[1], pixel[2], 2] <- runif(1, 0.3, 0.8)
-    
     ############### Make pixel a random color #########
     x[pixel[1], pixel[2], 1] <- runif(1)
     x[pixel[1], pixel[2], 2] <- runif(1)
     x[pixel[1], pixel[2], 3] <- runif(1)
-    
-    
-    ########Make Pixel Average Color of Image #######
-    # y[pixel[1], pixel[2], 1] <- mean_R
-    # y[pixel[1], pixel[2], 2] <- mean_G
-    # y[pixel[1], pixel[2], 3] <- mean_B
-    
-    ################ Make Pixels Yellow ###############
-    # y[pixel[1], pixel[2], 1:2] <- runif(1, 0.78, 1)
-    # 
-    # y[pixel[1], pixel[2], 3] <- 0
   }
   return(x)
 }
 
 # identifies the most average color pixels out of the image
 change_most_average <- function(y, pixel_budget=0.01) {
-    mean_R <- mean(y[,,1])
-    mean_G <- mean(y[,,2])
-    mean_B <- mean(y[,,3])
-    
-    dist_from_avg <- abs(y[,,1] - mean_R) + abs(y[,,2] - mean_G) + abs(y[,,3] - mean_B)
-    cutoff <- quantile(dist_from_avg, pixel_budget, type=1)
-    most_avg <- which(dist_from_avg < cutoff, arr.ind = TRUE)
-    for (i in 1:dim(most_avg)[1]) {
-      pixel <- most_avg[i,]
-      
-      ################ Make Pixel Green-ish #############
-      # y[pixel[1], pixel[2], 1] <- runif(1, 0, 0.1)
-      # y[pixel[1], pixel[2], 2] <- runif(1, 0.3, 0.8)
-      
-      ############### Make pixel a random color #########
-      y[pixel[1], pixel[2], 1] <- runif(1)
-      y[pixel[1], pixel[2], 2] <- runif(1)
-      y[pixel[1], pixel[2], 3] <- runif(1)
-      
-      
-      ########Make Pixel Average Color of Image #######
-      # y[pixel[1], pixel[2], 1] <- mean_R
-      # y[pixel[1], pixel[2], 2] <- mean_G
-      # y[pixel[1], pixel[2], 3] <- mean_B
-      
-      ################ Make Pixels Yellow ###############
-      # y[pixel[1], pixel[2], 1:2] <- runif(1, 0.78, 1)
-      # 
-      # y[pixel[1], pixel[2], 3] <- 0
-    }
-    return(y)
-}
-
-# identifies the least average color pixels out of the image
-change_least_average <- function(y, pixel_budget=0.01) {
+  
+  # averages for each color value in image
   mean_R <- mean(y[,,1])
   mean_G <- mean(y[,,2])
   mean_B <- mean(y[,,3])
   
+  # calculates how far away each pixel's color is from the average image color
   dist_from_avg <- abs(y[,,1] - mean_R) + abs(y[,,2] - mean_G) + abs(y[,,3] - mean_B)
-  cutoff <- quantile(dist_from_avg, 1 - pixel_budget, type=1)
-  most_avg <- which(dist_from_avg > cutoff, arr.ind = TRUE)
+  
+  # finds indices of the most average pixels, limited to the pixel budget
+  cutoff <- quantile(dist_from_avg, pixel_budget, type=1)
+  most_avg <- which(dist_from_avg < cutoff, arr.ind = TRUE)
+  
+  # changes colors of the most average pixels
   for (i in 1:dim(most_avg)[1]) {
     pixel <- most_avg[i,]
-    
-    ################ Make Pixel Green-ish #############
-    # y[pixel[1], pixel[2], 1] <- runif(1, 0, 0.1)
-    # y[pixel[1], pixel[2], 2] <- runif(1, 0.3, 0.8)
+
+    ############### Make pixel a random color #########
+    y[pixel[1], pixel[2], 1] <- runif(1)
+    y[pixel[1], pixel[2], 2] <- runif(1)
+    y[pixel[1], pixel[2], 3] <- runif(1)
+  }
+  return(y)
+}
+
+# identifies the least average color pixels out of the image
+change_least_average <- function(y, pixel_budget=0.01) {
+  
+  # averages for each color value in image
+  mean_R <- mean(y[,,1])
+  mean_G <- mean(y[,,2])
+  mean_B <- mean(y[,,3])
+  
+  # calculates how far away each pixel's color is from the average image color
+  dist_from_avg <- abs(y[,,1] - mean_R) + abs(y[,,2] - mean_G) + abs(y[,,3] - mean_B)
+  
+  # finds indices of the least average pixels, limited to the pixel budget
+  cutoff <- quantile(dist_from_avg, 1 - pixel_budget, type=1)
+  least_avg <- which(dist_from_avg > cutoff, arr.ind = TRUE)
+  
+  # changes colors of the least
+  for (i in 1:dim(least_avg)[1]) {
+    pixel <- least_avg[i,]
     
     ############### Make pixel a random color #########
     y[pixel[1], pixel[2], 1] <- runif(1)
     y[pixel[1], pixel[2], 2] <- runif(1)
     y[pixel[1], pixel[2], 3] <- runif(1)
-    
-    
-    ########Make Pixel Average Color of Image #######
-    # y[pixel[1], pixel[2], 1] <- mean_R
-    # y[pixel[1], pixel[2], 2] <- mean_G
-    # y[pixel[1], pixel[2], 3] <- mean_B
-    
-    ################ Make Pixels Yellow ###############
-    # y[pixel[1], pixel[2], 1:2] <- runif(1, 0.78, 1)
-    # 
-    # y[pixel[1], pixel[2], 3] <- 0
   }
   return(y)
 }
+
+
+
+
+# not used
+# reduceUpperExtremities <- function(x, pixel_budget) {
+#   cutoff <- quantile(x, 1-pixel_budget, type=1)
+#   print(cutoff)
+#   ext_removed <- apply(x, 1, function(r){r[r > cutoff] <- 0})
+#   return(ext_removed)
+# }
--- orig_model.R
+++ orig_model.R
@@ -12,6 +12,16 @@ install_tensorflow(extra_packages="pillow")
 ## RUN SEPARATELY ##
 install_keras()
 
+
+
+
+# this whole file is pretty messy I'll clean it up tomorrow
+
+
+
+
+
+
 model <- load_model_tf("dandelion_model")
 
 res=c("","")
@@ -39,67 +49,35 @@ y <- y/255
 source("algos.R")
 ############### Find "most green" pixels in image ####################
 pixel_budget <- 0.01
-y_inv <- y
+
 #y_inv <- convert_mostGreen(y, pixel_budget = pixel_budget)
-y_inv <- convert_mostYellow(y, pixel_budget = pixel_budget)
+
+#y_inv <- convert_mostYellow(y, pixel_budget = pixel_budget)
+y_inv <- y
+y_inv <- mod_image(y, 0.1)
+# y_inv <- change_least_average(y)
+plot(1:dim(y)[1], 1:dim(y)[2], type = "n", xlab = "", ylab = "", axes = FALSE)
+rasterImage(as.raster(y_inv), 1, 1, dim(y)[1], dim(y)[2])
 y_test <- array_reshape(y_inv, c(1, dim(y_inv)))
 model %>% predict(y_test)
-plot(1:224, 1:224, type = "n")
-rasterImage(as.raster(y_inv), 1, 1, 224, 224)
-
-##########################################################################
-
-y_inv <- y
-#y_inv[,,1] <- abs(y_inv[,,1] - y_inv[,,2])
-#y_inv[abs(y[,,1] - y[,,2]) < 0.1] <- 0
-low <- floor(45 * dim(y_inv)[1] / 100)
-hi <- floor(55* dim(y_inv)[1] / 100)
-ind_seq <- seq(from=0, to=dim(y)[1], by=10)
-y_inv[ind_seq, ind_seq, 1:3] <- runif(3)
-y_inv[low:hi, low:hi, ] <- 0.8
-#y_inv[low:hi, low:hi, 3] <- 0
-#y_inv[,,1] <- reduceUpperExtremities(y[,,1], 0.99)
-y <- array_reshape(y, c(1, dim(y)))
-model %>% predict(y)
-
-y_rast <- as.raster(y_inv)
-plot(1:224, 1:224, type = "n")
-rasterImage(y_rast, 1, 1, 224, 224)
-
-
-x <- image_to_array(test_image)
-x <- array_reshape(x, c(1, dim(x)))
-x <- x/255
-pred <- model %>% predict(x)
-
-
-##################### fgsm attempt #############################
-
-#label <- tf$one_hot(208)
-tf$keras$Model$predict(model, x)
-tensor_image <- to_tensor(paste("grass/",f_grass[1],sep=""))
-labels <- tf$constant(c(as.integer(1), as.integer(2)), shape=c(as.integer(1), as.integer(2)), dtype=tf$int32)
-loss_obj <- tf$keras$losses$CategoricalCrossentropy(model)
-perturbation <- fgsm_adversify(tf$reshape(tensor_image, shape=c(as.integer(1), as.integer(224), as.integer(224), as.integer(3))), labels, loss_obj)
-perturbation <- tf$reshape(perturbation, shape=c(as.integer(224), as.integer(224), as.integer(3)))
-
-display_image_tensor(perturbation * 0.5 + 0.5)
-print(pred)
-
 
 #############################################################################
 
+num_fooled <- 0
 for (i in f_grass){
   test_image <- image_load(paste("grass/",i,sep=""),
                            target_size = target_size)
   x <- image_to_array(test_image)
-  x <- array_reshape(x, c(1, dim(x)))
   x <- x/255
+  x <- mod_image(x, 0.05)
+  x <- array_reshape(x, c(1, dim(x)))
   pred <- model %>% predict(x)
   if(pred[1,2]<0.50){
-    print(i)
+    num_fooled <- num_fooled + 1
+    #print(i)
   }
 }
+print(num_fooled)
 
 res=c("","")
 num_fooled = 0
@@ -108,16 +86,33 @@ for (i in f_dande){
                            target_size = target_size)
   x <- image_to_array(test_image)
   x <- x/255
-  x <- convert_mostYellow(x, 0.01)
+  #x <- convert_mostGreen(x)
+  x <- mod_image(x, pixel_budget = 0.05)
   x_test <- array_reshape(x, c(1, dim(x)))
   pred <- model %>% predict(x_test)
   print(pred)
   if(pred[1,1]<0.50){
     # print(i)
-    plot(1:224, 1:224, type = "n")
-    rasterImage(as.raster(x), 1, 1, 224, 224)
+    # plot(1:224, 1:224, type = "n")
+    # rasterImage(as.raster(x), 1, 1, 224, 224)
     num_fooled = num_fooled + 1
   }
 }
 print(num_fooled)
 print(res)
+
+
+
+##################### fgsm attempt #############################
+
+#label <- tf$one_hot(208)
+# tf$keras$Model$predict(model, x)
+# tensor_image <- to_tensor(paste("grass/",f_grass[1],sep=""))
+# labels <- tf$constant(c(as.integer(1), as.integer(2)), shape=c(as.integer(1), as.integer(2)), dtype=tf$int32)
+# loss_obj <- tf$keras$losses$CategoricalCrossentropy(model)
+# perturbation <- fgsm_adversify(tf$reshape(tensor_image, shape=c(as.integer(1), as.integer(224), as.integer(224), as.integer(3))), labels, loss_obj)
+# perturbation <- tf$reshape(perturbation, shape=c(as.integer(224), as.integer(224), as.integer(3)))
+# 
+# display_image_tensor(perturbation * 0.5 + 0.5)
+# print(pred)
+
