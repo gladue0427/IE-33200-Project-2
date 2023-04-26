4c23717f8f50842addd0af1ae2ec1345c49bcd90
matt4ta <57694978+matt4ta@users.noreply.github.com>
2023-04-26 00:58:45 -0400

    started adding method to generate examples of how images are changed

--- orig_model.R
+++ orig_model.R
@@ -19,8 +19,8 @@ install_keras()
 
 
 
-
-
+source("algos.R")
+source("mod_image.R")
 
 model <- load_model_tf("dandelion_model")
 
@@ -33,6 +33,8 @@ rgb <- 3 #color channels
 f_grass <- list.files("grass")
 f_dande <- list.files("dandelions")
 
+pixel_budget <- 0.05
+
 ############################## TEST CODE ############################
 test_image <- image_load("grass",
                          target_size = target_size)
@@ -41,25 +43,22 @@ test_image <- image_load("grass",
 #                          target_size = target_size)
 
 #y <- image_load(paste("grass/",f_grass[1],sep=""), target_size = target_size)
-y <- image_load(paste("dandelions/",f_dande[1],sep=""),
+image_name <- paste("dandelions/",f_dande[1],sep="")
+y <- image_load(image_name,
                 target_size = target_size)
 y <- image_to_array(y)
 y <- y/255
-
-source("algos.R")
-############### Find "most green" pixels in image ####################
-pixel_budget <- 0.01
-
-#y_inv <- convert_mostGreen(y, pixel_budget = pixel_budget)
-
-#y_inv <- convert_mostYellow(y, pixel_budget = pixel_budget)
+plot(1:dim(y)[1], 1:dim(y)[2], main=paste("Image: ",image_name,"\nNo Changes "), type = "n", xlab = "", ylab = "", axes = FALSE)
+rasterImage(as.raster(y), 1, 1, dim(y)[1], dim(y)[2])
 y_inv <- y
-y_inv <- mod_image(y, 0.1)
+y_inv <- mod_image(y, pixel_budget = pixel_budget)
 # y_inv <- change_least_average(y)
-plot(1:dim(y)[1], 1:dim(y)[2], type = "n", xlab = "", ylab = "", axes = FALSE)
-rasterImage(as.raster(y_inv), 1, 1, dim(y)[1], dim(y)[2])
+plot(1:dim(y_inv)[1], 1:dim(y_inv)[2], main=paste("Image: ",image_name,"\nPixel Budget: ", pixel_budget), type = "n", xlab = "", ylab = "", axes = FALSE)
+rasterImage(as.raster(y_inv), 1, 1, dim(y_inv)[1], dim(y_inv)[2])
 y_test <- array_reshape(y_inv, c(1, dim(y_inv)))
-model %>% predict(y_test)
+pred <- model %>% predict(y_test)
+print(pred)
+
 
 #############################################################################
 
@@ -87,14 +86,15 @@ for (i in f_dande){
   x <- image_to_array(test_image)
   x <- x/255
   #x <- convert_mostGreen(x)
-  x <- mod_image(x, pixel_budget = 0.05)
+  x <- mod_image(x, pixel_budget = pixel_budget)
   x_test <- array_reshape(x, c(1, dim(x)))
   pred <- model %>% predict(x_test)
   print(pred)
   if(pred[1,1]<0.50){
     # print(i)
     # plot(1:224, 1:224, type = "n")
-    # rasterImage(as.raster(x), 1, 1, 224, 224)
+    # plot(1:dim(x)[1], 1:dim(x)[2], main=paste("Image: ",i,"\nPixel Budget: ", pixel_budget), type = "n", xlab = "", ylab = "", axes = FALSE)
+    # rasterImage(as.raster(x), 1, 1, dim(x)[1], dim(x)[2])
     num_fooled = num_fooled + 1
   }
 }
